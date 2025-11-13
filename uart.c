84501883e8ea50d01d0bd3299e29ce246aba99a4
Ved Yeole <vedsyeole@gmail.com>
2025-11-13 11:11:25 -0600

    added main func header to uart.c

--- uart.c
+++ uart.c
@@ -1,4 +1,12 @@
 // defining uart registers
 #define UART_BASE
-#define UART_DR
-#define UART_FR
+#define UART_DR UART_BASE + 0x000
+#define UART_FR UART_BASE + 0x018
+#define UART_CR UART_BASE + 0x030
+
+int main() {
+    // enable uart
+    
+
+
+}
