d48e3c04e5afc6d2fa2442715353dcc0eda017ee
Ved Yeole <vedsyeole@gmail.com>
2025-11-19 13:22:10 -0600

    main os code

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "uart.h"
+
+void main(void) {
+    uart_init(); // literally does nothing because qemu pre-initializes it, but have this line for good practice
+    uart_puts("Hello World!\nHowdy World!");
+    
+    while(1); //infinite loop so we don't leave the OS
+}
